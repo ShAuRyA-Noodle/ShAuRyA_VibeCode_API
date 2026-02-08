a814ba5e0096fd2c2b4f0b18c90c9d702e19880a
ShAuRyA <paneermomos10@gmail.com>
2026-02-08 18:20:00 +0530

    vercel serverless fix

--- index.js
+++ index.js
@@ -5,15 +5,10 @@ import express from "express";
 import bodyParser from "body-parser";
 import axios from "axios";
 
-
-
 const app = express();
-const port = 3000;
-
 
 const WEATHER_API_KEY = process.env.WEATHER_API_KEY;
 const NEWS_API_KEY = process.env.NEWS_API_KEY;
-
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -32,13 +27,9 @@ function formatPopulation(num) {
     return num;
 }
 
-
 app.get("/", (req, res) => {
-    res.render("index.ejs", {
-        country: null
-    });
+    res.render("index.ejs", { country: null });
 });
-
 
 app.post("/search", async (req, res) => {
     const countryName = req.body.country;
@@ -50,22 +41,20 @@ app.post("/search", async (req, res) => {
 
         const results = response.data;
 
-        // try to find exact match
         const countryData =
             results.find(c =>
                 c.name.common.toLowerCase() === countryName.toLowerCase()
-            ) || results[0];   // fallback if exact match not found
+            ) || results[0];
 
         const capital = countryData.capital[0];
         const currencyCode = Object.keys(countryData.currencies)[0];
+
         const currencyResponse = await axios.get(
             `https://open.er-api.com/v6/latest/USD`
         );
 
         const rate = currencyResponse.data.rates[currencyCode];
         const formattedRate = rate.toFixed(2);
-
-
 
         const weatherResponse = await axios.get(
             `https://api.openweathermap.org/data/2.5/weather?q=${capital}&units=metric&appid=${WEATHER_API_KEY}`
@@ -86,7 +75,6 @@ app.post("/search", async (req, res) => {
 
         const articles = newsResponse.data.articles;
 
-
         res.render("index.ejs", {
             country: countryData,
             formattedPopulation: formatPopulation(countryData.population),
@@ -96,16 +84,10 @@ app.post("/search", async (req, res) => {
             news: articles
         });
 
-
     } catch (error) {
-        res.render("index.ejs", {
-            country: null
-        });
+        res.render("index.ejs", { country: null });
     }
 });
 
-
-
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-});
+/* IMPORTANT FOR VERCEL */
+export default app;
